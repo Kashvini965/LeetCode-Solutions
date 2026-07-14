1954f3222b2ef50074c1092e08a31e0e32b95ba7
Kashvini965 <145687503+Kashvini965@users.noreply.github.com>
2026-07-14 14:01:44 +0530

    Create 3397-FindTheIntegerAddedToArrayI

---
+++ 3397-FindTheIntegerAddedToArrayI/3397-FindTheIntegerAddedToArrayI.java
@@ -0,0 +1,8 @@
+// Last updated: 7/14/2026, 2:01:43 PM
+class Solution {
+    public int addedInteger(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        return nums2[0]-nums1[0];
+    }
+}
