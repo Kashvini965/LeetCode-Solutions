3f7496f9ad2014ac532125c5a05a731bd5b9b957
Kashvini965 <145687503+Kashvini965@users.noreply.github.com>
2026-07-14 14:02:48 +0530

    Create 1797-GoalParserInterpretation

---
+++ 1797-GoalParserInterpretation/1797-GoalParserInterpretation.java
@@ -0,0 +1,20 @@
+// Last updated: 7/14/2026, 2:02:47 PM
+class Solution {
+    public String interpret(String command) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<command.length();i++){
+            char ch=command.charAt(i);
+            if(ch=='G'){
+                sb.append('G');
+            }else if(ch=='(' && command.charAt(i+1)==')'){
+                sb.append('o');
+                i++;
+            }    
+            else{
+                sb.append("al");
+                i+=3;
+            }
+        }
+        return sb.toString();
+    }
+}
