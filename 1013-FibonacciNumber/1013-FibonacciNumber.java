f8047b3eb6e661b99b73906e57e54e1af899b297
Kashvini965 <145687503+Kashvini965@users.noreply.github.com>
2026-07-14 14:03:13 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.java
@@ -0,0 +1,13 @@
+// Last updated: 7/14/2026, 2:03:12 PM
+class Solution {
+    public int fib(int n) {
+        if(n==0) return 0;
+        int a=0,b=1;
+        for(int i=2;i<=n;i++){
+            int temp=b;
+            b=a+b;
+            a=temp;
+        }
+        return b;
+    }
+}
